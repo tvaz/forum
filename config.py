4cca7fa1df3bc6fd3937b508188c5800b78ee907
tvaz <victoriavaz24@gmail.com>
2018-04-27 14:04:08 -0700

    added config

---
+++ config.py
@@ -0,0 +1,33 @@
+# config.py
+
+
+class Config(object):
+    """
+    Common configurations
+    """
+    SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Put any configurations here that are common across all environments
+
+
+class DevelopmentConfig(Config):
+    """
+    Development configurations
+    """
+
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+
+
+class ProductionConfig(Config):
+    """
+    Production configurations
+    """
+
+    DEBUG = False
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
